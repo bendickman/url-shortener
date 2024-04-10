82b3f7b1bf902d50c2e710b5412eb75455b8072c
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2024-04-10 16:55:19 +0100

    add url validity check

--- src/UrlShortener.Api/Controllers/UrlShortenerController.cs
+++ src/UrlShortener.Api/Controllers/UrlShortenerController.cs
@@ -16,6 +16,11 @@ public class UrlShortenerController : Controller
     public IActionResult Index(
         [FromBody] string url)
     {
+        if (!Uri.TryCreate(url, UriKind.Absolute, out var uri))
+        {
+            return BadRequest("Not a valid URL");
+        }
+
         var result = _urlShortenerService.ShortenUrl(url);
 
         if (string.IsNullOrEmpty(result))
